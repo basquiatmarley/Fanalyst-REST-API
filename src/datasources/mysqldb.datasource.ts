230e2d891440c2950ce1d67e026cd5e57ee4b92d
basquiatmarley <brandon@nghbr.app>
2024-05-07 10:36:09 +0700

    CHANGE DB CONFIGURATION

--- src/datasources/mysqldb.datasource.ts
+++ src/datasources/mysqldb.datasource.ts
@@ -7,6 +7,9 @@ const config = {
   connector: 'mysql',
   url: `mysql://${process.env.DB_USER || 'root'}:${process.env.DB_PASSWORD || 'root'}@${process.env.DB_HOST || 'localhost'}:${process.env.DB_PORT || '3306'}/${process.env.DB_NAME || 'fanalyst'}`,
   parseJsonColumns: true,
+  sequelizeOptions: {
+    dialectOptions: {}
+  }
 }
 @lifeCycleObserver('datasource')
 export class MysqldbDataSource extends SequelizeDataSource
